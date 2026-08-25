796badd0791ccfab246841a5eadc3e40851a4f75
Fabian Luebbe <fabian.luebbe@gmail.com>
2026-08-25 12:18:47 +0200

    home/git: add lg1/lg2 pretty-format log aliases
    The plain --oneline graph was hard to scan for author/date; lg1/lg2
    add colorized relative/absolute date variants, with lg defaulting to
    lg1.

--- home/git.nix
+++ home/git.nix
@@ -25,10 +25,13 @@
         st = "status -sb";
         co = "checkout";
         br = "branch";
-        lg = "log --oneline --graph --decorate --all";
         last = "log -1 HEAD --stat";
         unstage = "reset HEAD --";
         amend = "commit --amend --no-edit";
+
+        lg1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
+        lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+        lg = "lg1";
       };
     };
 
